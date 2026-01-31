ea86a054f6e5a128d3e5291f8d72fe932fff4e3e
therealteejay25 <therealteejay25@gmail.com>
2026-01-31 11:30:20 +0100

    mdcfs

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         <Image src="/f.png" alt="Logo" width={60} height={60} className="hidden md:block dark:opacity-65 absolute bottom-30 left-40" />
         <Image src="/g.png" alt="Logo" width={60} height={60} className="hidden md:block dark:opacity-65 absolute top-30 right-10" />
         <div className="flex flex-col z-30 items-center">
-          <motion.div
+          {/* <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.1 }}
@@ -60,7 +60,7 @@ export default function Home() {
               <Image src="/tayo.png" alt="Logo" width={26} height={26} />
             </div>
             <p className="text-dark/50 dark:text-white/50 font-medium">Trusted by users worldwide.</p>
-          </motion.div>
+          </motion.div> */}
           <div className="flex items-center justify-center mt-8 flex-col max-w-4xl text-center gap-4">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
