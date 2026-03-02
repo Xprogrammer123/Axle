feb1070f113b1da25ec61fe5975999d4278c71bc
Xprogrammer123 <fawassaka862@gmail.com>
2026-03-02 04:19:19 +0100

    google auth complete

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -459,7 +459,7 @@ const Page = () => {
                 Sign Up
               </Button>
             </StaggerItem>
-{/*
+
             <StaggerItem>
               <div className="flex items-center gap-3 w-full mt-4">
                 <div className="h-[1px] bg-dark/10 dark:bg-white/10 flex-1" />
@@ -496,7 +496,7 @@ const Page = () => {
                 </span>
               </Button>
             </StaggerItem>
-            */}
+       
           </form>
 
           <StaggerItem>
